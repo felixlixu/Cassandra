25a0380cdf688e0878bc1f5485bb0026989a2d6f
felixlixu <233349922@qq.com>
2014-05-15 14:34:32 +0800

    Commit
    commit

---
+++ Test/src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	static int f=0;
+	static {
+		f++;
+	}
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Test1.f);
+	}
+
+}
